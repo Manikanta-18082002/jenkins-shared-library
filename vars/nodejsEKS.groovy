2697a212dd3ecc174a8f16eb233490138b2e0ddd
Mani Kanta <konugantimanikantareddy@gmail.com>
2024-07-21 19:39:50 +0530

    Added some text

--- vars/nodejsEKS.groovy
+++ vars/nodejsEKS.groovy
@@ -1,64 +1,63 @@
 def call(Map configMap){
     pipeline {
-    agent {
-        label 'AGENT-1'
-    }
-    options {
-        timeout(time: 30, unit: 'MINUTES')
-        disableConcurrentBuilds() // No Multiple  Builds
-        ansiColor('xterm')
-    }
-    environment{
-        def appVersion = '' // variable declaration in GLOBAL LEVEL
-        nexusUrl = pipelineGlobals.nexusURL()
-        region = pipelineGlobals.region()
-        account_id = pipelineGlobals.account_id()
-        component = configMap.get("component")
-        project = configMap.get("project")
-        // def releaseExists = ""
-    }
-    stages {
-        stage ('read the version'){
-            steps{ // Variable can be accessed with in that stage only
-                script{ // Groovy Script
-                def packageJson = readJSON file: 'package.json'
-                appVersion = packageJson.version
-                echo "application version: $appVersion"
+        agent {
+            label 'AGENT-1'
+        }
+        options {
+            timeout(time: 30, unit: 'MINUTES')
+            disableConcurrentBuilds()
+            ansiColor('xterm')
+        }
+        environment{
+            def appVersion = '' //variable declaration
+            nexusUrl = pipelineGlobals.nexusURL()
+            region = pipelineGlobals.region()
+            account_id = pipelineGlobals.account_id()
+            component = configMap.get("component")
+            project = configMap.get("project")
+            def releaseExists = ""
+        }
+        stages {
+            stage('read the version'){
+                steps{
+                    script{
+                        echo sh(returnStdout: true, script: 'env')
+                        def packageJson = readJSON file: 'package.json'
+                        appVersion = packageJson.version
+                        echo "application version: $appVersion"
+
+                    }
                 }
             }
-        }
-        stage('Install Dependencies') { // init should happen whether apply or destroy
-            steps {
-               sh """
-                npm install
-                ls -ltr
-                echo "application version: $appVersion"
-               """
-            }
-        }
-        stage('Build'){ // Build == Dependencies + code (zipped)
-            steps{
+            stage('Install Dependencies') {
+                steps {
                 sh """
-                sudo yum install zip -y
-                zip -q -r ${component}-${appVersion}.zip * -x Jenkinsfile -x ${component}-${appVersion}.zip
-                ls -ltr
+                    npm install
+                    ls -ltr
+                    echo "application version: $appVersion"
                 """
-            } // -q (quit --> No need of un-necessary log in jenkins )   -x exclude those files
-        }
-
-        stage('Docker build'){
-            steps{
-                sh """
-                    aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${region}.amazonaws.com
-
-                    docker build -t ${account_id}.dkr.ecr.${region}.amazonaws.com/${project}-${component}:${appVersion} .
-
-                    docker push ${account_id}.dkr.ecr.${region}.amazonaws.com/${project}-${component}:${appVersion}
-                """
+                }
             }
-        }
+            stage('Build'){
+                steps{
+                    sh """
+                    zip -q -r ${component}-${appVersion}.zip * -x Jenkinsfile -x ${component}-${appVersion}.zip
+                    ls -ltr
+                    """
+                }
+            }
+            stage('Docker build'){
+                steps{
 
-        stage('Deploy'){
+                    sh """
+                        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${region}.amazonaws.com
+                        docker build -t ${account_id}.dkr.ecr.${region}.amazonaws.com/${project}-${component}:${appVersion} .
+                        docker push ${account_id}.dkr.ecr.${region}.amazonaws.com/${project}-${component}:${appVersion}
+                    """
+                }
+            }
+
+            stage('Deploy'){
                 steps{
                     script{
                         releaseExists = sh(script: "helm list -A --short | grep -w ${component} || true", returnStdout: true).trim()
@@ -83,7 +82,6 @@ def call(Map configMap){
                     }
                 }
             }
-
             stage('Verify Deployment'){
                 steps{
                     script{
@@ -114,45 +112,40 @@ def call(Map configMap){
                     }
                 }
             }
-
-
-
-
-        //  stage('Nexus Artifact Upload'){
-        //     steps{
-        //         script{ // Groovy Script for Jenkins
-        //             nexusArtifactUploader( // --> This is plugin below code from internet
-        //                 nexusVersion: 'nexus3',
-        //                 protocol: 'http',
-        //                 nexusUrl: "${nexusUrl}", // double quotes --> When using variables
-        //                 groupId: 'com.expense',
-        //                 version: "${appVersion}",
-        //                 repository: "backend",
-        //                 credentialsId: 'nexus-auth', 
-        //                 // Created in Jenkins -->Manage Jenkins --> Credentials --> System --> Global credentials (unrestricted)
-        //                 artifacts: [
-        //                     [artifactId: "backend",
-        //                     classifier: '',
-        //                     file: "backend-" + "${appVersion}" + '.zip', // filename: backend-1.1.0.zip
-        //                     type: 'zip']
-        //                 ]
-        //             )
-        //         }
-        //     }
-        // }
-
-     }
-    post {  //This will catch the event and send Alerts to Mail/Slack
-        always { 
-            echo 'I will always say Hello again!'
-            deleteDir()
+            
+            /* stage('Nexus Artifact Upload'){
+                steps{
+                    script{
+                        nexusArtifactUploader(
+                            nexusVersion: 'nexus3',
+                            protocol: 'http',
+                            nexusUrl: "${nexusUrl}",
+                            groupId: 'com.expense',
+                            version: "${appVersion}",
+                            repository: "backend",
+                            credentialsId: 'nexus-auth',
+                            artifacts: [
+                                [artifactId: "backend" ,
+                                classifier: '',
+                                file: "backend-" + "${appVersion}" + '.zip',
+                                type: 'zip']
+                            ]
+                        )
+                    }
+                }
+            } */
         }
-        success { 
-            echo 'I will run when pipeline is success'
-        }
-        failure { 
-            echo 'I will run when pipeline is failure'
+        post { 
+            always { 
+                echo 'I will always say Hello again!'
+                deleteDir()
+            }
+            success { 
+                echo 'I will run when pipeline is success'
+            }
+            failure { 
+                echo 'I will run when pipeline is failure'
+            }
         }
     }
-}
 }
